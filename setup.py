c46f48de9a753d60cf3b2a0aa1be679718b8a992
Avrumy <contact@lungers.com>
2025-09-17 08:52:29 +0300

    Update setup info

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
+import re
+
 from setuptools import find_packages, setup
 
 with open('README.md') as f:
     long_description = f.read()
 
+with open('ini/__init__.py', 'r', encoding='utf-8') as f:
+    version = re.search(r"^__version__ = '(.*?)'$", f.read(), flags=re.MULTILINE).group(1)
+
 setup(
     name='ini-parser',
-    version='1.2.1',
+    version=version,
     description='An ini parser/serializer in Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/YouTwitFace/ini-parser',
-    author='Andrew Lane',
+    url='https://github.com/vrumger/ini-parser',
+    author='Avrumy Lunger',
     author_email='contact@lungers.com',
     license='MIT',
     packages=find_packages()
